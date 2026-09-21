f3f86ec1d7dee2d2150b8df744c72c2b0175d3e2
vapee <mehdi33889@gmail.com>
2026-09-21 13:48:21 +0200

    Chat Listener changes and fixes

--- src/main/java/dev/vapee/core/chat/ChatListener.java
+++ src/main/java/dev/vapee/core/chat/ChatListener.java
@@ -13,22 +13,27 @@ public final class ChatListener implements Listener {
 
     private final ChatService chatService;
     private final SocialService socialService;
-    private final ChatRenderer chatRenderer;
 
     public ChatListener(ChatService chatService, SocialService socialService) {
         this.chatService = Objects.requireNonNull(chatService, "chatService");
         this.socialService = Objects.requireNonNull(socialService, "socialService");
-        this.chatRenderer = ChatRenderer.viewerUnaware(this.chatService::render);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onAsyncChat(AsyncChatEvent event) {
         event.viewers().removeIf(viewer -> viewer instanceof Player player
-                && socialService.isIgnoring(player.getUniqueId(), event.getPlayer().getUniqueId())
+                        && socialService.isIgnoring(
+                        player.getUniqueId(),
+                        event.getPlayer().getUniqueId()
+                )
         );
+
         if (!chatService.isEnabled()) {
             return;
         }
-        event.renderer(chatRenderer);
+
+        event.renderer(
+                ChatRenderer.viewerUnaware(chatService::render)
+        );
     }
 }
